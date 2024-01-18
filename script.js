4448767943049a783058007ba278517237d92225
Hasan <148314926+Hasan-siddqui@users.noreply.github.com>
2024-01-18 17:22:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,3 +33,31 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 });
+
+// Testimonial slider
+$(document).ready(function () {
+    $('.my-slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        speed: 300,
+        infinite: true,
+        autoplaySpeed: 1000,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
