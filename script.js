c9b6353de7037f46be0c3552e1377db16d8390dc
Hasan <148314926+Hasan-siddqui@users.noreply.github.com>
2024-01-20 09:41:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
@@ -40,7 +40,7 @@ $(document).ready(function () {
         slidesToShow: 6,
         slidesToScroll: 1,
         arrows: true,
-        dots: true,
+        dots: false,
         speed: 300,
         infinite: true,
         autoplaySpeed: 1000,
@@ -61,3 +61,29 @@ $(document).ready(function () {
         ]
     });
 });
+
+
+// MY WORK
+
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+function prev(){
+    document.getElementById('slider-container-1').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container-1').scrollLeft += 270;
+}
+
+
+// $(".slide img").on("click" , function(){
+// $(this).toggleClass('zoomed');
+// $(".overlay").toggleClass('active');
+// })
